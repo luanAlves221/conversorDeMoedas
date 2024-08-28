d81db20fb817a3afc04d5328579e8bcd0bb38b83
luanAlves221 <luanlisboa220@gmail.com>
2024-08-28 00:09:00 -0300

    feito e arrumado o arquivo .js, e o index também

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,11 +1,54 @@
-function converterMoeda() {
-    const moedaOrigem = document.getElementById('moedaOrigem').value;
-    const moedaDestino = document.getElementById('moedaDestino').value;
+async function carregarMoedas() {
+    const url = "https://open.er-api.com/v6/latest/USD";
+    const response = await fetch(url);
+    const dados = await response.json();
+    const moedas = Object.keys(dados.rates);
+    
+    const s_moeda_o = document.getElementById("moedaOrigem");
+    const s_moeda_d = document.getElementById("moedaDestino");
+
+    moedas.forEach(codigo => {
+        let opcao = new Option(codigo, codigo);
+        s_moeda_o.add(opcao.cloneNode(true));
+        s_moeda_d.add(opcao);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    carregarMoedas();
+});
+
+async function calc(moeda_o, moeda_d, valor) {
+    const url = `https://open.er-api.com/v6/latest/${moeda_o}`;
+    const response = await fetch(url);
+    const dados = await response.json();
+
+    const taxa = dados.rates[moeda_d];
+    return (valor * taxa).toFixed(2);
+}
+
+function s_historico(o, d, quantidade, resultado) {
+    let historico = JSON.parse(localStorage.getItem('historicoC')) || [];
+    historico.unshift({ o: o, d: d, quantidade: quantidade, resultado: resultado });
+    historico = historico.slice(0, 10);
+    localStorage.setItem('historicoC', JSON.stringify(historico));
+}
+
+async function converterMoeda() {
+    const moeda_o = document.getElementById('moedaOrigem').value;
+    const moeda_d = document.getElementById('moedaDestino').value;
     const valor = document.getElementById('valor').value;
 
-    if (!moedaOrigem || !moedaDestino || !valor || valor <= 0) {
+    if (!moeda_o || !moeda_d || !valor || valor <= 0) {
         document.getElementById('resultado').innerText = 'Por favor. Insira um valor Válido e selecione as moedas';
         return;
     }
+
+    try {
+        const resultado = await calc(moeda_o, moeda_d, valor);
+        document.getElementById('resultado').innerText = `R$ ${resultado}`;
+        s_historico(moeda_o, moeda_d, valor, resultado);
+    } catch (erro) {
+        document.getElementById('resultado').innerText = 'Erro ao tentar realizar a conversão. Tente novamente mais tarde.';
+    }
 }
-//é necessario fazer a requisição para o backEnd
